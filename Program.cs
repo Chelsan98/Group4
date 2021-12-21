fbe24ea433ccfdf667c8ff7d0bb17a1667f50848
Akraftt <90035332+Akraftt@users.noreply.github.com>
2021-12-21 10:50:47 +0100

    new

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Group4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
