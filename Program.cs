06ec7353c1d940906cfcd6070c40d6616dbdb956
Chelsan98 <96055501+Chelsan98@users.noreply.github.com>
2021-12-22 13:32:33 +0100

    städning

--- Program.cs
+++ Program.cs
@@ -20,88 +20,75 @@ namespace Group4
             Console.WriteLine("3 = Time");
             Console.WriteLine("4 = Area eller volym");
             Console.Write("| Choose: ");
-            int opinion = int.Parse(Console.ReadLine());
-            switch(opinion)
-            {
-                case 1:
-                Console.WriteLine("Ohms Lag");
-                int opinionOhm = int.Parse(Console.ReadLine());
-                switch(opinionOhm){
-                    case 1:
-                    Console.WriteLine("R * I = U");
-                    break;
-                    case 2:
-                    Console.WriteLine("U / R = I");
-                    break;
-                    case 3:
-                    Console.WriteLine("U / I = R");
-                    break;
-                }
-                case 2:
+            var opinion = (Console.ReadLine());
+                if (opinion == "1")
+                {
+                  
+                        Console.WriteLine("Ohms Lag");
+                        var opinionOhm = (Console.ReadLine());
+                    {
+
+
+                        Console.WriteLine("R * I = U");
+
+
+                        Console.WriteLine("U / R = I");
+
+
+                        Console.WriteLine("U / I = R");
+                    }
+                      
+                        
+                
+               
                 Console.WriteLine("Grader");
-                int opinionGrad = int.Parse(Console.ReadLine());
-                switch(opinionGrad){
-                    case 1:
+                var opinionGrad = (Console.ReadLine());
+              
                     Console.WriteLine("Celius");
-                    break;
-                    case 2:
+                 
                     Console.WriteLine("Kelvin");
-                    break;
-                    case 3:
+                  
                     Console.WriteLine("Fahrenheit");
-                    break;
+                  
                 }
-                case 3:
+                
                 Console.WriteLine("Times");
-                int opinionTime = int.Parse(Console.ReadLine());
-                switch(opinionTime){
-                    case 1:
-                    case 2:
-                    case 3:
-                }
-                case 4:
+                var opinionTime = (Console.ReadLine());
+               
+                
+               
                 Console.WriteLine("Volym eller area?");
-                int opinionVolArea = int.Parse(Console.ReadLine());
-                switch(opinionVolArea){
-                    case 1:
+                var opinionVolArea = (Console.ReadLine());
+              
                     Console.WriteLine("Volym");
-                    int opinionVolym = int.Parse(Console.ReadLine());
-                    switch(opinionVolym){
-                        case 1:
+                    var opinionVolym = (Console.ReadLine()); 
+                  
                         Console.WriteLine("Rectangle Volym");
-                        break;
-                        case 2:
+                       
                         Console.WriteLine("Triangle Volym");
-                        break;
-                        case 3:
+                      
                         Console.WriteLine("Cricle Volym");
-                        break;
-                        case 4:
+                      
                         Console.WriteLine("Cube Volym");
-                        break;
-                        default:
+                     
                         Console.WriteLine("error");
-                    }
-                    case 2:
+                    
+                 
                     Console.WriteLine("Area");
-                    int opinionArea = int.Parse(Console.ReadLine());
-                        switch(opinionArea){
-                        case 1:
+                    var opinionArea = (Console.ReadLine());
+                { 
+                      
                         Console.WriteLine("Rectangle Volym");
-                        break;
-                        case 2:
+                       
                         Console.WriteLine("Triangle Area");
-                        break;
-                        case 3:
+                       
                         Console.WriteLine("Cricle Area");
-                        break;
-                        case 4:
+                       
                         Console.WriteLine("Cube Area");
-                        break;
-                        default:
+                       
                         Console.WriteLine("error");
                     }
-                    default:
+                
                 }
 
 
